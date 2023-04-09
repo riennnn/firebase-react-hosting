0344677c5e03dde3e187605e076334b53a78a571
rie.n <x1111.rie@gmail.com>
2023-04-09 11:52:14 +0900

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Button, Table} from 'reactstrap';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import TodosContextProvider from './contexts/TodosContext';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className='mt-4'>Todo リスト</h1>
+        <TodosContextProvider>
+          <TodoForm />
+          <TodoList />
+        </TodosContextProvider>
+      </Container>
     </div>
   );
 }
---
+++ src/contexts/TodosContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useState} from "react"
+
+export const TodosContext = createContext()
+
+const TodosContextProvider = (props) => {
+  const [todos, setTodos] = useState([])
+  const addTodo = text => {
+    const newTodos = [...todos, {text, complete: false}]
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos) 
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].complete = !newTodos[index].complete
+    // ↑わからない
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
+  return (
+    <TodosContext.Provider value={{todos, addTodo, removeTodo, completeTodo}}>
+      {props.children}
+    </TodosContext.Provider>
+  )
+}
+
+export default TodosContextProvider
